be2370cfaf76edb6ac1828cd70a57faf67bd8c52
Gaddmb <gaddielmb@gmail.com>
2025-06-06 16:37:37 +0200

    Fin du project

--- src/components/TableLine.jsx
+++ src/components/TableLine.jsx
@@ -1,5 +1,10 @@
+import CoinChart from "./CoinChart";
 import PercentChange from "./PercentChange";
+import StarIcon from "./StarIcon";
+import { useState } from "react";
 const TableLine = ({ coin, index }) => {
+  // On utilise useState pour gérer l'état de l'affichage du graphique si on le montre ou pas
+  const [showChart, setShowChart] = useState(false);
   const priceFormater = (num) => {
     // On transforme le nombre en string pour compter combien de chiffres il a .Si le nombre est petit (moins de 4 chiffres), on applique un joli format avec décimales
 
@@ -22,14 +27,21 @@ const TableLine = ({ coin, index }) => {
   return (
     <div className="table-line">
       <div className="infos-container">
-        <span></span>
+        <StarIcon coinId={coin.id} />
         <p>{index + 1}</p>
         <div className="img">
           <img src={coin.image} height="20 " alt="logo" />
         </div>
         <div className="infos">
-          <div className="chart-img">
+          <div
+            className="chart-img"
+            onMouseEnter={() => setShowChart(true)}
+            onMouseLeave={() => setShowChart(false)}
+          >
             <img src="./assets/chart-icon.svg" alt="chart-icon" />
+            <div className="chart-container" id={coin.name}>
+              {showChart && <CoinChart coinId={coin.Id} coinName={coin.name} />}
+            </div>
           </div>
           <h4>{coin.name}</h4>
           <span>
